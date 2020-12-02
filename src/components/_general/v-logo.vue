9db9dbf5b248577673612030bec91e14c981f345
emilJS777 <e.pargevich@mail.ru>
2020-12-02 14:25:22 +0400

    changes

--- src/components/_general/v-logo.vue
+++ src/components/_general/v-logo.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link to="/" class="image_href">
 <!--    <img :src="require('@/assets/img/demoLogo.png')" alt="">-->
-    <h3><span>Crypto</span><span>Buy</span></h3>
+    <h3><span class="bg">K7</span><span>Crypto</span></h3>
   </router-link>
 </template>
 
@@ -21,17 +21,24 @@ name: "v-logo"
     color: #0b364f;
     font-weight: 900;
     letter-spacing: -2px;
+
+
   }
-  span:first-child{
+  span.bg{
     background-color: #42a5a5;
     color: #fff;
     padding: 1px 5px;
     font-weight: bold;
     border-top-right-radius: 10px 20px;
     border-bottom-left-radius: 10px 20px;
+  @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+    font-family: 'Lobster', cursive;
+
   }
   span:last-child{
     margin-left: 0px;
+    @import url('https://fonts.googleapis.com/css2?family=Syne+Mono&display=swap');
+      font-family: 'Syne Mono', monospace;
   }
   .image_href > img{
     width: 80px;
